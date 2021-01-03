262e36d8f71e46378e76826a231f1e632bf51550
PUSKÁS Mátyás <matyaspuskas76@protonmail.com>
2021-01-03 19:44:29 +0000

    Changement pas important dans main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
   public static void main(String[] args) {
 
     BlocAnimal animal = new BlocAnimal();
-    CaseVisible case2 = new CaseVisible();
+    //CaseVisible case2 = new CaseVisible();
 
     Tableau tableau = new Tableau(2,2);
     tableau.remplirTableauInvisible(0,0);
